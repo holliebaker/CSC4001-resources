52ebeda5813957b8fdad3b0afb661f3b0e7093ed
Hollie Baker <hollieabaker@gmail.com>
2026-09-16 10:54:26 +0100

    Update create index script
    Searches for CSC4001 files, compiles the list by name and session number

--- create-index.sh
+++ create-index.sh
@@ -2,16 +2,17 @@
 
 cat <<EOF > index.qmd
 ---
-title: "Web Dev 1"
+title: "CSC4001: Systems and Architecture"
 ---
 
 # Index
 
 EOF
 
-for f in $(ls -v1 WD1*.qmd); do
-  n=$(grep "subtitle" $f | sed -e 's/subtitle: "\(.*\)"$/\1/g')
-  echo "- [$n](./$f)" >> index.qmd
+for f in $(ls -v1 CSC4001-*.qmd); do
+  number=$(echo "$f" | sed -e "s/CSC4001\-\(S[[:digit:]]\+\\)-.*/\1/g")
+  name=$(grep "title" $f | sed -e 's/title: "\(.*\)"$/\1/g')
+  echo "- ${number}: [$n](./$f)" >> index.qmd
 done
 
 echo >> index.qmd
